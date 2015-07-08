dee831b8e7e168c70de331afab82722c51aceef8
unknown <jpaukov@FIN00490GU16869.kih.kmart.com>
2015-07-08 17:06:02 -0500

    almost done with lesson 1

--- Lesson1.py
+++ Lesson1.py
@@ -8,12 +8,11 @@ Created on Thu Jul 02 13:52:33 2015
 import os
 
 DATADIR = "C:\Users\jpaukov\Documents\Udacity\mongoDB\mongoDB_lesson_1_2429"
-DATAFILE = "Beatles_diskography_3.csv"
-
+DATAFILE = "Beatles_diskography.csv"
 
 def parse_file(datafile):
     data = []
-    with open(datafile, "r") as f:
+    with open(datafile, "rb") as f:
         header = f.readline().split(",")
         counter = 0
         for line in f:
@@ -24,22 +23,28 @@ def parse_file(datafile):
             entry = {}
             
             for i, value in enumerate(fields):
-                entry[header[i]] = value.strip()
+                entry[header[i].strip()] = value.strip()   
                 
-                data.append(entry)
-                counter +=1
-
+            data.append(entry)
+            counter +=1
+                
     return data
 
 
 def test():
     # a simple test of your implemetation
     datafile = os.path.join(DATADIR, DATAFILE)
-    d = parse_file(datafile)
-    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
-    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+    d = parse_file(datafile)    
+    
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22-Mar-63', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10-Jul-64', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
 
     assert d[0] == firstline
     assert d[9] == tenthline
-
+    print d[0]
+    print firstline
+    print d[9]
+    print tenthline    
+    
+    
 test()
