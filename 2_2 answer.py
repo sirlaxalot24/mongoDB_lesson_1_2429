266e36e11fb85b83ee763d4d09987d78738cd222
unknown <jpaukov@FIN00490GU16869.kih.kmart.com>
2015-09-24 08:01:32 -0500

    new answers

---
+++ 2_2 answer.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 15:09:02 2015
+
+@author: jpaukov
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# All your changes should be in the 'extract_airports' function
+# It should return a list of airport codes, excluding any combinations like "All"
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html)
+        aL = soup.find(id="AirportList")
+        for option in aL.find_all('option'):
+            if option['value'] != 'All' and option['value'] != 'AllOthers' and option['value'] != 'AllMajors':
+                data.append(option['value'])
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    print data
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+test()
---
+++ 2_3 answer.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 15:52:00 2015
+
+@author: jpaukov
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Let's assume that you combined the code from the previous 2 exercises
+# with code from the lesson on how to build requests, and downloaded all the data locally.
+# The files are in a directory "data", named after the carrier and airport:
+# "{}-{}.html".format(carrier, airport), for example "FL-ATL.html".
+# The table with flight info has a table class="dataTDRight".
+# There are couple of helper functions to deal with the data files.
+# Please do not change them for grading purposes.
+# All your changes should be in the 'process_file' function
+# This is example of the datastructure you should return
+# Each item in the list should be a dictionary containing all the relevant data
+# Note - year, month, and the flight data should be integers
+# You should skip the rows that contain the TOTAL data for a year
+# data = [{"courier": "FL",
+#         "airport": "ATL",
+#         "year": 2012,
+#         "month": 12,
+#         "flights": {"domestic": 100,
+#                     "international": 100}
+#         },
+#         {"courier": "..."}
+# ]
+from bs4 import BeautifulSoup
+from zipfile import ZipFile
+import os
+
+datadir = "data"
+
+
+def open_zip(datadir):
+    with ZipFile('{0}.zip'.format(datadir), 'r') as myzip:
+        myzip.extractall()
+
+
+def process_all(datadir):
+    files = os.listdir(datadir)
+    return files
+
+
+def process_file(f):
+    """This is example of the data structure you should return.
+    Each item in the list should be a dictionary containing all the relevant data
+    from each row in each file. Note - year, month, and the flight data should be 
+    integers. You should skip the rows that contain the TOTAL data for a year
+    data = [{"courier": "FL",
+            "airport": "ATL",
+            "year": 2012,
+            "month": 12,
+            "flights": {"domestic": 100,
+                        "international": 100}
+            },
+            {"courier": "..."}
+    ]
+    """
+    data = []
+    info = {}
+    info["courier"], info["airport"] = f[:6].split("-")
+    # Note: create a new dictionary for each entry in the output data list.
+    # If you use the info dictionary defined here each element in the list 
+    # will be a reference to the same info dictionary.
+    with open("{}/{}".format(datadir, f), "r") as html:
+
+        soup = BeautifulSoup(html)
+        
+        #MY CODE:
+                
+        DataGrid1 = soup.find(id = 'DataGrid1')
+        for tr in DataGrid1.find_all('tr')[1:]:
+            flghts={}            
+            td = tr.find_all('td')
+            if td[1].string != "TOTAL":
+                info['year'] = int(td[0].string)
+                info['month'] = int(td[1].string)
+                flghts['domestic'] = int(td[2].string.replace(",",""))
+                flghts['international'] = int(td[3].string.replace(",",""))
+                info['flights'] = flghts
+                data.append(info)        
+        
+    return data
+
+
+def test():
+    print "Running a simple test..."
+    open_zip(datadir)
+    files = process_all(datadir)
+    data = []
+    for f in files:
+        data += process_file(f)
+        
+    assert len(data) == 399  # Total number of rows
+    for entry in data[:3]:
+        assert type(entry["year"]) == int
+        assert type(entry["month"]) == int
+        assert type(entry["flights"]["domestic"]) == int
+        assert len(entry["airport"]) == 3
+        assert len(entry["courier"]) == 2
+    assert data[0]["courier"] == 'FL'
+    assert data[0]["month"] == 10
+    assert data[-1]["airport"] == "ATL"
+    assert data[-1]["flights"] == {'international': 108289, 'domestic': 701425}
+    
+    print "... success!"
+
+if __name__ == "__main__":
+.    test()
